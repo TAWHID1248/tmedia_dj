93f5455c8e2ddb02446b58b501515c2072d0c45c
tawhid1248 <frzfardin2020@gmail.com>
2024-10-09 19:50:40 +0600

    second commit

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("Home Page")
-
-def room(request):
-    return HttpResponse("ROom")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # Homepage URL
-    path('room/', room),  # Homepage URL
+    path('', include('base.urls'))
 ]
