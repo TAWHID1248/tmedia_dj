ab211c9adc8a5e6e2553b9dde95ebd513f9110f7
tawhid1248 <frzfardin2020@gmail.com>
2024-10-10 19:47:51 +0600

    3rd commit

--- base/views.py
+++ base/views.py
@@ -6,8 +6,35 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Home Page")
+rooms = [
+    {
+        'id': 1,
+        'name': 'Room 1',
+        'description': 'This is Room 1',
+        'price': 200
+    },
+    {
+        'id': 2,
+        'name': 'Room 2',
+        'description': 'This is Room 2',
+        'price': 300
+    },
+    {
+        'id': 3,
+        'name': 'Room 3',
+        'description': 'This is Room 3',
+        'price': 400
+    }
+]
 
-def room(request):
-    return HttpResponse("Room")
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
